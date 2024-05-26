54c557b8a9bc51c83ca47473d74cd1fe8d78c548
Abdulla-Al-Ehsan <155327393+Abdulla-Al-Ehsan@users.noreply.github.com>
2024-05-26 13:17:10 +0600

    Update Bubble-sort.cpp

--- Algorithm/Sorting/Bubble-sort.cpp
+++ Algorithm/Sorting/Bubble-sort.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+   int Swap( int *a, int *b)
+   {
+       int tmp = *a;
+       *a=*b;
+       *b=tmp;
+   }
+
+     int main()
+     {
+        int n; 
+        cin>>n;
+        int ar[n];
+        for(int i=0; i<n; i++)  cin>>ar[i];
+        
+
+         for(int i=0; i<n; i++)
+         { 
+
+            for(int j=0; j<n; j++)
+              { 
+                 if(ar[j]>ar[j+1])
+                 {
+                    Swap( &ar[j], &ar[j+1]);
+                 }
+              }
+
+         }
+
+
+          for(int i=0; i<n; i++)
+         {
+            cout<<ar[i]<<" ";
+ 
+}
+     }
 
